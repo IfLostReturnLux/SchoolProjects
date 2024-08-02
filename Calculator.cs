b0ca9c57ce1e393aa6dca83a1ed1888698e2b845
Valevskiy <104253175+IfLostReturnLux@users.noreply.github.com>
2024-08-02 15:34:38 +0300

    Test Calculator

---
+++ Calculator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+        public float Add(float first, float second)
+        {
+            float result = first + second;
+            return result;
+        }
+        public float Subtract(float first, float second)
+        {
+            float result = first - second;
+            return result;
+        }
+        public float Multiply(float first, float second)
+        {
+            float result = first * second;
+            return result;
+        }
+    }
+}
